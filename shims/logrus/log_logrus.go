f4893fb49741dac056f3123e1dcd7e495352e1da
Tatsuro Alpert <tatsuro@invisionapp.com>
2018-03-04 09:19:59 -0800

    simplify

--- shims/logrus/log_logrus.go
+++ shims/logrus/log_logrus.go
@@ -6,7 +6,7 @@ import (
 )
 
 type shim struct {
-	l *logrus.Entry
+	*logrus.Entry
 }
 
 // NewLogrus can be used to override the default logger.
@@ -17,54 +17,14 @@ func NewLogrus(logger *logrus.Logger) log.Logger {
 		logger = logrus.StandardLogger()
 	}
 
-	return &shim{l: logrus.NewEntry(logger)}
-}
-
-// Debug log message
-func (s *shim) Debug(msg ...interface{}) {
-	s.l.Debug(msg...)
-}
-
-// Info log message
-func (s *shim) Info(msg ...interface{}) {
-	s.l.Info(msg...)
-}
-
-// Warn log message
-func (s *shim) Warn(msg ...interface{}) {
-	s.l.Warn(msg...)
-}
-
-// Error log message
-func (s *shim) Error(msg ...interface{}) {
-	s.l.Error(msg...)
-}
-
-// Debugf log message with formatting
-func (s *shim) Debugf(format string, args ...interface{}) {
-	s.l.Debugf(format, args...)
-}
-
-// Infof log message with formatting
-func (s *shim) Infof(format string, args ...interface{}) {
-	s.l.Infof(format, args...)
-}
-
-// Warnf log message with formatting
-func (s *shim) Warnf(format string, args ...interface{}) {
-	s.l.Warnf(format, args...)
-}
-
-// Errorf log message with formatting
-func (s *shim) Errorf(format string, args ...interface{}) {
-	s.l.Errorf(format, args...)
+	return &shim{logrus.NewEntry(logger)}
 }
 
 // WithFields will return a new logger based on the original logger with
 // the additional supplied fields. Wrapper for logrus Entry.WithFields()
 func (s *shim) WithFields(fields log.Fields) log.Logger {
 	cp := &shim{
-		l: s.l.WithFields(logrus.Fields(fields)),
+		s.Entry.WithFields(logrus.Fields(fields)),
 	}
 	return cp
 }
